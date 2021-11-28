c078cd45679e18ef0122d41e844fcf158e581812
idev-emrul <emrul.hasan@itlabsolutions.com>
2021-11-28 12:02:32 +0600

    Print layout with header footer and wattermark first commit

---
+++ test_invoice.php
@@ -0,0 +1,315 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="print.css">
+
+    <title>Hello, world!</title>
+  </head>
+
+<style>
+	.headerGroup:after {
+		content: 'Text';
+		background-image: url('Moto-invoice-setup-96-watter-mark.jpg');
+	}
+</style>
+<body>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+	<table width="100%">
+		<thead>
+			<tr>
+				<td class="headerGroup">
+					<div class="header-block"></div>
+				</td>
+			</tr>
+		</thead>
+		<tfoot>
+			<tr>
+				<td class="footerGroup">
+        <div class="page">
+        	<table>
+        		<thead>
+        			<tr>
+        				<td> sn</td>
+        				<td> head ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</td>
+        			</tr>
+        		</thead>
+        		<tbody>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>14</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>15</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>16</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			<tr>
+        				<td>01</td>
+        				<td>tehs</td>
+        			</tr>
+        			
+        		</tbody>
+        	</table>
+
+        </div>
+       
+      </td>
+    </tr>
+  </tbody>
+</table>
+</div>
+
+<!-- ============ srtart absolute header/footer image========== -->
+<div class="header">
+	<img src="Moto-invoice-setup-96-Header.jpg" alt="header image" width="100%" height="150px" style="float:left; background-size: cover;">
+</div>
+<div class="footer">
+	<img src="Moto-invoice-setup-96-Footer.jpg" alt="Footer image" width="100%" height="120px" style="float:left; background-size: cover;">
+</div>
+<!-- ============ end absolute header/footer image========== -->
+
+  <!-- Optional JavaScript; choose one of the two! -->
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
